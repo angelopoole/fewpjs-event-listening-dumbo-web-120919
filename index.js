42beb41d85d9d7bdafc651f498c5fa630414b95c
angelopoole <angelo.poole@outlook.com>
2020-02-03 11:54:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
-}
+const input = document.querySelector("#input")
+
+input.addEventListener('click', event => {
+    alert("I was clicked 🤮")
+})
+
+
+// function addingEventListener(click) {
+//     const input = document.getElementById('input');
+//     input.addEventListener('click', function(event) {
+//       alert('I was clicked!');
+//     });
+// }
